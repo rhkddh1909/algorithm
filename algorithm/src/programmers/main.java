106df45e475b7ab1ffcabb9e9868d441277cc5b8
leegwangoh <rhkddh1909@gmail.com>
2021-10-01 11:44:35 +0900

    외적구하

---
+++ algorithm/src/programmers/main.java
@@ -0,0 +1,10 @@
+package programmers;
+
+public class main {
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4};
+		int b[] = {-3,-1,0,2};
+		
+		new DotProduct().solution(a,b);
+	}
+}
