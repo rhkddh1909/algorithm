13e1f3b85237782603f079c7f867f17c2397bb82
leegwangoh <rhkddh1909@gmail.com>
2021-10-23 15:16:42 +0900

    한수 문제추가

---
+++ algorithm/src/baekjoon/Hansoo.java
@@ -0,0 +1,23 @@
+package baekjoon;
+import java.util.*;
+public class Hansoo {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		int count = 0;
+		if(num >1000) {
+			while(num <=100)
+				num = s.nextInt();
+		}
+		for(int i = 1; i<=num; i++) {
+			if(i<100) {
+				count++;
+			}
+			else if(((i/100)-((i%100)/10)) == (((i%100)/10) -(i%100)%10))
+			{
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+}
