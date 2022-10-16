f19a2f121b4c74dbce2de6839873adbc1bac03fd
이광오 <rhkddh1909@gmail.com>
2022-10-16 23:52:13 +0900

    2022 10 16 programers

---
+++ algorithm/src/programmers/FractionSum.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class FractionSum {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int[] answer = {};
+        return answer;
+    }
+}
