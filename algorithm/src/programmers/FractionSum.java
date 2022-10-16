4e6f31b7b73133451db5c83db75e6e3acff719ac
이광오 <rhkddh1909@gmail.com>
2022-10-17 00:08:40 +0900

    2022 10 17 programers

--- algorithm/src/programmers/FractionSum.java
+++ algorithm/src/programmers/FractionSum.java
@@ -1,8 +1,27 @@
 package programmers;
 
+import java.util.*;
+
 public class FractionSum {
-    public int[] solution(int denum1, int num1, int denum2, int num2) {
-        int[] answer = {};
+    public List<Integer> solution(int denum1, int num1, int denum2, int num2) {
+        List<Integer> answer = new ArrayList<>();
+        int sumDenum = denum1*num2 + denum2*num1;
+        int sumNum = num1 * num2;
+
+        int idx = sumNum > sumDenum ? sumNum : sumDenum;
+        int i = 2;
+        while(i <= idx){
+            if(sumDenum%i == 0 && sumNum%i ==0){
+                sumDenum = sumDenum/i;
+                sumNum = sumNum/i;
+            }
+            else {
+                i++;
+            }
+        }
+        answer.add(sumDenum);
+        answer.add(sumNum);
+
         return answer;
     }
 }
