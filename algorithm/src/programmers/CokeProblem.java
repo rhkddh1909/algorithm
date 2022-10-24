d361596989abd1bb105e7b8f93fac10474b5fed9
이광오 <rhkddh1909@gmail.com>
2022-10-24 20:12:27 +0900

    2022 10 24 programers java8

--- algorithm/src/programmers/CokeProblem.java
+++ algorithm/src/programmers/CokeProblem.java
@@ -3,6 +3,11 @@ package programmers;
 public class CokeProblem {
     public int solution(int a, int b, int n) {
         int answer = 0;
+        int coke = n;
+        while(coke/a != 0){
+            answer += (coke/a) * b;
+            coke = (coke/a) * b + coke %a;
+        }
 
         return answer;
     }
