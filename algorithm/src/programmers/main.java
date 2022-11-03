80ea44af0feeaa1975f91242b0ff76f70ab95152
이광오 <rhkddh1909@gmail.com>
2022-11-03 10:41:08 +0900

    2022 10 25 programers java8 무의미 커밋

--- algorithm/src/programmers/main.java
+++ algorithm/src/programmers/main.java
@@ -2,8 +2,8 @@ package programmers;
 
 public class main {
 	public static void main(String[] args) {
-		int[] arr = {1};
-		
-			System.out.println(new Mode().solution(arr));
+		int[][] a = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] b = {1,5,3,5,1,2,1,4,4};
+		System.out.println(new ParenthesisRation().solution("[](){}"));
 	}
 }
